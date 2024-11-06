e6f75c422e0b61593aca4476b69964fc8e7cbae2
Marcos8060 <marcosgav80@gmail.com>
2024-11-07 01:20:53 +0300

    polished admin panel

---
+++ src/app/components/admin-panel/delete-edit-user.js
@@ -0,0 +1,68 @@
+"use client";
+import { fetchAllUsers } from "@/app/redux/features/users";
+import { deleteUser } from "@/app/redux/services/users";
+import { useAuth } from "@/assets/hooks/use-auth";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import { useState } from "react";
+import toast from "react-hot-toast";
+import { BsThreeDots } from "react-icons/bs";
+import { useDispatch } from "react-redux";
+
+const DeleteEditUser = ({ user }) => {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  const dispatch = useDispatch();
+  const token = useAuth();
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  
+
+  const clearUser = async () => {
+    const response = await deleteUser(user?.id, token);
+    if (response === "Request failed with status code 404") {
+      toast.error("User not found");
+    } else {
+      dispatch(fetchAllUsers(token));
+      toast.success(response.message);
+    }
+  };
+
+  return (
+    <div>
+      <BsThreeDots onClick={handleClick} className="cursor-pointer text-sm" />
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "left",
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "left",
+        }}
+        MenuListProps={{
+          "aria-labelledby": "basic-button",
+        }}
+      >
+        <MenuItem>
+          <p className="text-xs">Edit</p>
+        </MenuItem>
+        <MenuItem>
+          <p onClick={()=> clearUser()} className="text-xs">Delete</p>
+        </MenuItem>
+      </Menu>
+    </div>
+  );
+};
+
+export default DeleteEditUser;
--- src/app/components/admin-panel/users-table.js
+++ src/app/components/admin-panel/users-table.js
@@ -5,20 +5,9 @@ import { useAuth } from "@/assets/hooks/use-auth";
 import { useDispatch } from "react-redux";
 import { fetchAllUsers } from "@/app/redux/features/users";
 import toast from "react-hot-toast";
+import DeleteEditUser from '../../components/admin-panel/delete-edit-user'
 
 export default function UsersTable({ users }) {
-  const dispatch = useDispatch();
-  const token = useAuth();
-
-  const clearUser = async (id) => {
-    const response = await deleteUser(id, token);
-    if (response === "Request failed with status code 404") {
-      toast.error("User not found");
-    } else {
-      dispatch(fetchAllUsers(token));
-      toast.success(response.message);
-    }
-  };
 
   return (
     <div className="">
@@ -26,13 +15,6 @@ export default function UsersTable({ users }) {
         <table className="table-auto w-full border-collapse bg-white shadow-xl rounded-bl rounded-br">
           <thead className="bg-primary rounded-xl text-background px-4 text-xs">
             <tr>
-              {/* <th className="p-2 font-thin rounded-tl text-left">
-                <input
-                  id="checkbox-table-search-1"
-                  type="checkbox"
-                  className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600"
-                />
-              </th> */}
               <th className="p-2 text-left">Username</th>
               <th className="p-2 text-left">Email</th>
               <th className="p-2 text-left">Role</th>
@@ -42,11 +24,6 @@ export default function UsersTable({ users }) {
               <th className="p-2  text-left">Gender</th>
               <th className="p-2  text-left ">Department</th>
               <th className="p-2  text-left ">Action</th>
-              {/* <th className=" p-2 font-thin text-left rounded-tr">Job Title</th> */}
-              {/* <th className=" ">Employment Type</th> */}
-              {/* <th className=" ">Employment Status</th> */}
-              {/* <th className=" ">Supervisor</th> */}
-              {/* <th className=" ">Bank Account</th> */}
             </tr>
           </thead>
           <tbody>
@@ -57,13 +34,6 @@ export default function UsersTable({ users }) {
                   index % 2 === 0 ? "bg-white" : "bg-gray-50"
                 } text-xs border border-background`}
               >
-                {/* <td className=" p-3">
-                  <input
-                    id="checkbox-table-search-1"
-                    type="checkbox"
-                    className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600"
-                  />
-                </td> */}
                 <td className=" p-2 flex items-center gap-2">
                   <img
                     className="h-8 w-8 rounded-full object-cover"
@@ -80,16 +50,12 @@ export default function UsersTable({ users }) {
                 <td className=" p-2">{user.profile?.gender}</td>
                 <td className=" p-2">{user.profile?.department}</td>
                 <td
-                  className=" p-2 text-warning font-bold cursor-pointer"
-                  onClick={() => clearUser(user.id)}
+                  className=" p-2 font-bold cursor-pointer"
+                  // onClick={() => clearUser(user.id)}
                 >
-                  Delete
+                  <DeleteEditUser {...{ user }} />
+                  {/* <BsThreeDots className="text-sm cursor-pointer" /> */}
                 </td>
-                {/* <td className=" p-2">{user.profile?.jobTitle}</td> */}
-                {/* <td className=" p-2">{user.profile?.employmentType}</td> */}
-                {/* <td className=" p-2">{user.profile?.employmentStatus}</td> */}
-                {/* <td className=" p-2">{user.profile?.supervisor}</td> */}
-                {/* <td className=" p-2">{user.profile?.bankAccount}</td> */}
               </tr>
             ))}
           </tbody>
