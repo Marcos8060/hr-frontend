aa17593cdc1f23a71b10018fb71252fa22668c93
Marcos8060 <marcosgav80@gmail.com>
2024-11-11 11:16:40 +0300

    added react skeleton

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -8,23 +8,50 @@ import { fetchAllUsers } from "../redux/features/users";
 import LeaveGraph from "../components/employees/leave-graph";
 import { Vacancies } from "../components/vacancies";
 import UsersTable from "../components/admin-panel/users-table";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+import { fetchGeneralLeaveData } from "../redux/features/employees";
 
 const Dashboard = () => {
   const dispatch = useDispatch();
   const token = useAuth();
   const { users } = useSelector((store) => store.users);
+  const { allLeaves } = useSelector((store) => store.employee);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    if (token || users?.length === 0) {
+    if (token) {
       dispatch(fetchAllUsers(token));
     }
   }, [token]);
+
+  useEffect(() => {
+    if (token) {
+      dispatch(fetchGeneralLeaveData(token))
+        .then(() => {
+          setLoading(false);
+        })
+        .catch(() => {
+          setLoading(false);
+        });
+    }
+  }, [token]);
+
   return (
     <>
       <DashboardCards {...{ users }} />
       <div className="flex gap-2">
         <div className="w-9/12">
-          <LeaveGraph />
+          {loading ? (
+            <Skeleton
+              baseColor="#c0c0c0" 
+              highlightColor="#f0f0f0"
+              count={3}
+              height={100}
+            />
+          ) : (
+            <LeaveGraph />
+          )}
         </div>
         <div className="w-3/12 mt-4">
           <Vacancies />
